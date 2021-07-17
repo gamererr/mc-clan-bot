99d8fbf8b898355651ad2ea0d2bd0dcb41e91acc
gamererr <starkid2u@outlook.com>
2021-07-17 17:12:34 +0000

    clan kick for clan leaders

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ async def leave(ctx):
 @client.command()
 async def remove(ctx, role):
 
-	role:discord.Role = ctx.message.mentions[0]
+	role:discord.Role = ctx.message.role_mentions[0]
 	inClan:discord.Role = ctx.guild.get_role(865713278860656661)
 	clanBorder:discord.Role = ctx.guild.get_role(865819163087994911)
 	leader:discord.Role = ctx.guild.get_role(865992562364252163)
@@ -93,19 +93,53 @@ async def transfer(ctx, newOwner):
 	newOwner = ctx.mentions[0]
 
 	leader:discord.Role = ctx.guild.get_role(865992562364252163)
+	clanBorder:discord.Role = ctx.guild.get_role(865819163087994911)
+
+	if not leader in ctx.author.roles:
+		return
 
 	for x in ctx.guild.roles:
-		if x in ctx.author.roles and x in newOwner.roles and x.position <= clanBorder.position and x.position != 0:
+		if x in ctx.author.roles and x in newOwner.roles and x.position <= clanBorder.position and x.position != -1:
+			clan = x
+			break
+		else:
+			clan = None
+			break
+		
+	if clan == None:
+		await ctx.send(f"you and {newOwner.display_name} arent in the same clan")
+		return
+	else:
+		await ctx.author.remove_roles(leader)
+		await newOwner.add_roles(leader)
+		await ctx.send(f"transfered ownership of {clan.name} to {newOwner.display_name}")
+
+@client.command()
+async def kick(ctx, kicked):
+
+	kicked = ctx.message.mentions[0]
+
+	leader:discord.Role = ctx.guild.get_role(865992562364252163)
+	inClan:discord.Role = ctx.guild.get_role(865713278860656661)
+	clanBorder:discord.Role = ctx.guild.get_role(865819163087994911)
+
+	if ctx.author == kicked or not leader in ctx.author.roles:
+		return
+
+	for x in ctx.guild.roles:
+		if x in ctx.author.roles and x in kicked.roles and x.position == clanBorder.position:
 			clan = x
 			break
 		else:
 			clan = None
 		
-		if clan == None:
-			await ctx.send(f"you and {newOwner.display_name} arent in the same clan")
-			return
-		else:
-			await ctx.author.remove_roles(leader)
-			await newOwner.add_roles(leader)
+	if clan == None:
+		await ctx.send(f"you and {kicked.display_name} arent in the same clan")
+		return
+	else:
+		await kicked.remove_roles(clan)
+		await kicked.remove_roles(inClan)
+		await ctx.send(f"kicked {kicked.display_name} from {clan.name}")
+
 
 client.run(token)
